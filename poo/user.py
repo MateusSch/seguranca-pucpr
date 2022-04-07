5e44d9cca01f20a39d94e6723a36e1d5683e611b
Mateus Schneider <91134992+MateusSch@users.noreply.github.com>
2022-04-06 21:02:34 -0300

    Primeira atualização aula 7

--- poo/user.py
+++ poo/user.py
@@ -1,3 +1,6 @@
+import hashlib
+
+
 class User:
 
     def __init__(self, login, senha):
@@ -14,13 +17,19 @@ class User:
 
     @staticmethod
     def criar_conta():
+        print("\n*** O usuário e a senha deve ter no máximo 4 caracteres ***\n")
         criar_login = input("Criar login: ")
         criar_senha = input("Criar senha: ")
-        conta = User(criar_login, criar_senha)
-        with open("dados.txt", "w") as arquivo:
-            arquivo.write(f"{conta.login}\n")
-            arquivo.write(f"{conta.senha}")
-        arquivo.close()
+        if len(criar_login) > 4 or len(criar_senha) > 4:
+            print("Tente criar novamente!")
+            User.criar_conta()
+        else:
+            conta = User(criar_login, criar_senha)
+            with open("dados.txt", "w") as arquivo:
+                arquivo.write(f"{conta.login}\n")
+                arquivo.write(f"{conta.senha}")
+            arquivo.close()
+            User.cria_hash()
 
     @staticmethod
     def entrar():
@@ -34,7 +43,21 @@ class User:
         entrar_senha = input("Digite a senha: ")
 
         if entrar_login == usuario[0] and entrar_senha == usuario[1]:
-            print(f"***Bem vindo {entrar_login.title()}***")
+            print(f"*** Bem vindo {entrar_login.title()} ***")
         else:
             print("Usuário ou senha incorretos, tente novamente")
             User.entrar()
+
+    @staticmethod
+    def cria_hash():
+        arquivo = open("dados.txt", "r")
+        usuario = []
+        for linha in arquivo:
+            linha = linha.strip()
+            usuario.append(linha)
+        arquivo.close()
+        senha_bit = usuario[1].encode()
+        arquivo_hash = open("hash.txt", "w")
+        hash_senha = str(hashlib.md5(senha_bit).hexdigest())
+        arquivo_hash.writelines(hash_senha)
+        arquivo_hash.close()
